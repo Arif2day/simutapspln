bfb39342c8ce4bd9419473b68523091cbc9e84d0
unknown <arief2day@gmail.com>
2025-07-20 15:42:27 +0700

    docker

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use PDF;
 use iio\libmergepdf\Merger;
+use App\Models\Users;
 
 class BerandaController extends Controller
 {
     public function index() {        
+        $user = Users::all();
+        dd($user);
         $return = array();
         return view("Guest.beranda.index", compact([]));
     }
